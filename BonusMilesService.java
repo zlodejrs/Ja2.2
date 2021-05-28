5da6ff62bc0a1e660934dd304c74c306f838f027
zlodejrs <trotsergey@list.ru>
2021-05-28 15:52:48 +0300

    last

--- BonusMilesService.java
+++
@@ -1,9 +0,0 @@
-public class BonusMilesService {
-
-    public int calculate(int price) {
-        int miles = 20;
-        int milesAmount = price / miles;
-        return milesAmount;
-		
-    }
-}
